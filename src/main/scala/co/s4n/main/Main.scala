1b8d7302f04e69f54110240ab7dca90c11aa49c4
Daniel <danielbernal@seen4n.com>
2017-03-14 16:51:58 -0500

    fix main method to start the app

--- src/main/scala/co/s4n/main/Main.scala
+++ src/main/scala/co/s4n/main/Main.scala
@@ -3,29 +3,32 @@ package co.s4n.main
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import co.s4n.user.controller.UsersRoute
 import spray.json.DefaultJsonProtocol
+
+import scala.io.StdIn
 
 object Main extends SprayJsonSupport with DefaultJsonProtocol {
 
   def main(args: Array[String]): Unit = {
 
-    implicit val system = ActorSystem("my-system")
-
+    implicit val system = ActorSystem("simple-rest-system")
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val route =
-      UsersRoute.route
+    // Define route
+    val route: Route = UsersRoute.route
 
+    // Startup, and listen for requests
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    StdIn.readLine()
 
+    //Shutdown
     bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
   }
 
 }
